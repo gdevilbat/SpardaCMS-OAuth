2c19600ebd563ca5265118e1cdd5ce95dec2e830
maldinuribrahim <maldinuribrahim@gmail.com>
2021-10-11 14:06:37 +0700

    Initialize Oauth

---
+++ Database/Seeders/OauthDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Gdevilbat\SpardaCMS\Modules\Oauth\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class OauthDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
